2f99d21c908dd4970180533b2cbab4d6365f518a
MBWL22 <banegaslopezm@gmail.com>
2020-04-11 18:00:53 -0600

    add module ruteujs

---
+++ Frontend/Controladores/app.js
@@ -0,0 +1,8 @@
+//Funcionalidad para poder filtrar en la tabla
+$(document).ready(function() {
+    $('#RutaList').DataTable();
+} );
+
+$(document).ready(function() {
+    $('#lugarList').DataTable();
+} );
--- Proyecto-Ingenieria/Js/app.js
+++
@@ -1,8 +0,0 @@
-//Funcionalidad para poder filtrar en la tabla
-$(document).ready(function() {
-    $('#RutaList').DataTable();
-} );
-
-$(document).ready(function() {
-    $('#lugarList').DataTable();
-} );
